b49ab5b0b8f09ea52e2dcc0922dde496ad47e3c1
Katsuhiro Yamanaka <Yamanaka_Katsuhiro@ogis-ri.co.jp>
2022-03-22 13:25:18 +0900

    nautible-appをnautible-app-msに変更

--- build.cmd
+++ build.cmd
@@ -5,4 +5,4 @@ if not %ERRORLEVEL% == 0 (
 )
 
 :dockerbuild
-cmd /c "cd nautible-app-order-build & docker build -t %IMAGE% -f ./src/main/docker/Dockerfile.jvm ."
+cmd /c "cd nautible-app-ms-order-build & docker build -t %IMAGE% -f ./src/main/docker/Dockerfile.jvm ."
